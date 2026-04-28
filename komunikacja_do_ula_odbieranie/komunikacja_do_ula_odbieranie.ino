70c2df06595f96ed370b8a53dc5905ceedee00e0
PiotrGalazka <piotrgalazka@student.agh.edu.pl>
2026-04-28 23:53:58 +0200

    Zaimplementowanie odbierania

---
+++ komunikacja_do_ula_odbieranie/komunikacja_do_ula_odbieranie.ino
@@ -0,0 +1,33 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+#define SS 5
+#define RST 22
+#define DIO0 21
+
+void setup() {
+  Serial.begin(115200);
+  SPI.begin(23, 19, 18);
+  LoRa.setPins(SS,RST,DIO0);
+
+  while (!LoRa.begin(868E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+
+  LoRa.setSyncWord(0x12);
+  LoRa.setSpreadingFactor(12);
+  LoRa.setSignalBandwidth(125E3);
+  LoRa.setCodingRate4(8);
+  LoRa.enableCrc();
+}
+
+void loop() {
+  if (LoRa.parsePacket()) {
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+    Serial.printf("    (%d)\n", LoRa.packetRssi());
+  }
+
+}
